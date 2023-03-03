d11adfb5f82e138639cf0c13258c37823f69d9b1
swaralipathak <103195543+swaralipathak@users.noreply.github.com>
2023-03-03 20:00:20 +0530

    Add files via upload

---
+++ frontend/src/pages/About/About.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import './About.css'
+
+export default function About() {
+    return (
+        <div className="about-container">
+            <h1 className="about-heading">About</h1>
+            <div className="about-main">
+                <p className="about-content">
+                    This is an realtime AI based Yoga Trainer which detects your pose how well you are doing.
+                    
+                    This AI first predicts keypoints or coordinates of different parts of the body(basically where
+                    they are present in an image) and then it use another classification model to classify the poses if 
+                    someone is doing a pose and if AI detects that pose more than 95% probability and then it will notify you are 
+                    doing correctly(by making virtual skeleton green). I have used Tensorflow pretrained Movenet Model To Predict the 
+                    Keypoints and building a neural network top of that which uses these coordinates and classify a yoga pose.
+
+                </p>
+                <div className="developer-info">
+                    <h4>About Developer</h4>
+                    <p className="about-content">We are AI Enthusiastic, Content Creator, Tutor,
+                    </p>
+                    <h4>Contact</h4>
+                    <a href="https://www.instagram.com"><p className="about-content">Instagram</p></a>
+                    <a href="https://www.youtube.com"><p  className="about-content">Youtube</p></a>
+                    <a href="https://github.com"><p  className="about-content">GitHub</p></a>
+                </div>
+            </div>
+        </div>
+    )
+}
